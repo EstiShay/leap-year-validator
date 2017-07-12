200b1989c2f08dfaa369a14d6fdf856cfc81bd9e
Collin Person, Esti Shay and Maria Thomas <collin@>
2017-07-12 10:24:25 -0700

    revised html and JS to restyle output.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // Business logic
 var leapYear = function(year) {
-  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0){
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)){
     return true;
   } else {
     return false;
@@ -12,6 +12,13 @@ $(document).ready(function() {
     event.preventDefault();
     var year = $("input#year").val();
     var result = leapYear(year);
-    $("#result").text(result);
+    $(".year").text(year);
+
+    if (!result) {
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
+    $("#result").show();
   });
 });
